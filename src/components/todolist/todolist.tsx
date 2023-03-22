d20beaa99f908fde33c581c195ee5a8e2c121cec
aymericCliq <a.demange@cliqdigital.com>
2023-03-23 00:38:36 +0100

    feat(search): wip error when import

--- src/components/todolist/todolist.tsx
+++ src/components/todolist/todolist.tsx
@@ -1,55 +1,85 @@
-import React, { useEffect, useState } from "react";
-import "./todolist.css";
-import ImportJson from "../json/importJson/importJson";
-import ExportToJson from "../json/exportJson/exportJson";
+import React, { useEffect, useState } from 'react';
+import './todolist.css';
+import ImportJson from '../json/importJson/importJson';
+import ExportToJson from '../json/exportJson/exportJson';
 
-export interface Task {
+export interface Manga {
   url: string;
   manga: string;
   chapitre: string;
   validated: boolean;
 }
 
-interface EditTask {
+interface EditManga {
   text: string;
   chapitre: string;
-  index: number
+  index: number;
 }
 
 const TodoList: React.FC = () => {
-  const [item, setItem] = useState<Task[]>([]);
-  const [itemEnCours, setItemEnCours] = useState<Task>({url: "", manga: "", chapitre: "", validated: false});
-  const [editEnCours, setEditEnCours] = useState<EditTask>({index: -1, text: "", chapitre: ""});
+  const [item, setItem] = useState<Manga[]>([]);
+  const [searched, setSearched] = useState<Manga[]>([]);
+  const [itemEnCours, setItemEnCours] = useState<Manga>({
+    url: '',
+    manga: '',
+    chapitre: '',
+    validated: false,
+  });
+  const [editEnCours, setEditEnCours] = useState<EditManga>({
+    index: -1,
+    text: '',
+    chapitre: '',
+  });
   const [noList, setNoList] = useState<boolean>(false);
   const [sortOrder, setSortOrder] = useState<boolean>(false);
+  const [searching, setSearching] = useState<string>('');
 
   const keyPressInput = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === "Enter") {
+    if (e.key === 'Enter') {
       addElement();
     }
   };
 
   const addElement = () => {
-    if (itemEnCours.url === null || itemEnCours.url === "" || itemEnCours.chapitre === "" || itemEnCours.chapitre === null || itemEnCours.manga === "" || itemEnCours.manga === null) {
+    if (
+      itemEnCours.url === null ||
+      itemEnCours.url === '' ||
+      itemEnCours.chapitre === '' ||
+      itemEnCours.chapitre === null ||
+      itemEnCours.manga === '' ||
+      itemEnCours.manga === null
+    ) {
       return;
     }
-    setItem([...item ,{manga: itemEnCours.manga , url: itemEnCours.url , chapitre: itemEnCours.chapitre, validated: false, }]);
-    setItemEnCours({url: "", manga: "", chapitre: "", validated: false})
+    setItem([
+      ...item,
+      {
+        manga: itemEnCours.manga,
+        url: itemEnCours.url,
+        chapitre: itemEnCours.chapitre,
+        validated: false,
+      },
+    ]);
+    setItemEnCours({ url: '', manga: '', chapitre: '', validated: false });
   };
 
   const updateElement = (id: number, edit: any) => {
     const updatedText = [...item];
-    updatedText[id] = {...updatedText[id], url: edit.text, chapitre: edit.chapitre}
+    updatedText[id] = {
+      ...updatedText[id],
+      url: edit.text,
+      chapitre: edit.chapitre,
+    };
     setItem(updatedText);
-    setEditEnCours({index: -1, text: "", chapitre: ""})
+    setEditEnCours({ index: -1, text: '', chapitre: '' });
   };
 
-  const startEditElement = (task: Task, key: number) => {
-    setEditEnCours({index: key, text: task.url, chapitre: task.chapitre})
+  const startEditElement = (Manga: Manga, key: number) => {
+    setEditEnCours({ index: key, text: Manga.url, chapitre: Manga.chapitre });
   };
 
   const handleList = () => {
-    setNoList(!noList)
+    setNoList(!noList);
   };
 
   const deleteElement = (indexTodoToDelete: number) => {
@@ -57,12 +87,14 @@ const TodoList: React.FC = () => {
     newTodos.splice(indexTodoToDelete, 1);
     setItem(newTodos);
   };
-  
+
   const sortElement = () => {
-    const sortedItem = item.sort(function (a, b) {
-      setSortOrder(!sortOrder)
-      return sortOrder ? a.manga.toLowerCase().localeCompare(b.manga.toLowerCase()) : b.manga.toLowerCase().localeCompare(a.manga.toLowerCase());
-    })
+    const sortedItem = item.sort(function (item, tempItem) {
+      setSortOrder(!sortOrder);
+      return sortOrder
+        ? item.manga.toLowerCase().localeCompare(tempItem.manga.toLowerCase())
+        : tempItem.manga.toLowerCase().localeCompare(item.manga.toLowerCase());
+    });
     setItem([...sortedItem]);
   };
 
@@ -72,18 +104,36 @@ const TodoList: React.FC = () => {
     setItem(validateTodo);
   };
 
+  const searchingElement = () => {
+    console.log(`seaching :`, searching, item);
+    const searchedItem = item
+      .filter(value => value.manga === searching)
+      .map(answer => console.log(`answer :`, answer));
+    if (searchedItem !== undefined) {
+      setSearched(searchedItem);
+    }
+  };
+
+  const deleteSearch = () => {
+    setSearched([]);
+  };
+
+  useEffect(() => {
+    console.log(`searched :`, searched);
+  }, [searched]);
+
   return (
     <div>
       <h2>Manga List</h2>
       <div>
         <label htmlFor="vehicle1">Ta une liste ?</label>
-        <input type="checkbox" onChange={handleList}/>
+        <input type="checkbox" onChange={handleList} />
         {noList && (
           <div>
             <ImportJson setItem={setItem} />
           </div>
         )}
-      </div>      
+      </div>
 
       <div className="blockInput">
         Manga*
@@ -92,80 +142,175 @@ const TodoList: React.FC = () => {
           autoFocus
           placeholder="Manga"
           value={itemEnCours.manga}
-          onChange={(e) => setItemEnCours({...itemEnCours, manga: e.target.value})}
-          onKeyUp={(e) => keyPressInput(e)}
+          onChange={e =>
+            setItemEnCours({ ...itemEnCours, manga: e.target.value })
+          }
+          onKeyUp={e => keyPressInput(e)}
         />
         Url*
         <input
           type="url"
           placeholder="Url scan sans numéro"
           value={itemEnCours.url}
-          onChange={(e) => setItemEnCours({...itemEnCours, url: e.target.value})}
-          onKeyUp={(e) => keyPressInput(e)}
+          onChange={e =>
+            setItemEnCours({ ...itemEnCours, url: e.target.value })
+          }
+          onKeyUp={e => keyPressInput(e)}
         />
         Chapitre*
         <input
           type="number"
-          required
           placeholder="Chapitre"
           value={itemEnCours.chapitre}
-          onChange={(e) => setItemEnCours({...itemEnCours, chapitre: e.target.value})}
-          onKeyUp={(e) => keyPressInput(e)}
+          onChange={e =>
+            setItemEnCours({ ...itemEnCours, chapitre: e.target.value })
+          }
+          onKeyUp={e => keyPressInput(e)}
         />
         <button onClick={() => addElement()}>Ajouter un manga</button>
         <button onClick={sortElement}>Trier</button>
-
+        <div>
+          <input
+            type="text"
+            placeholder="Searching"
+            value={searching}
+            onChange={e => setSearching(e.target.value)}
+            onKeyUp={e => keyPressInput(e)}
+          />
+          <button onClick={searchingElement}>Searching</button>
+          <button onClick={deleteSearch}>&#x274C;</button>
+        </div>
       </div>
 
       <ul className="listItem">
-        {item.map((task, key) => (
-          <li
-            key={key}
-            className={`listItemElement ${task.validated ? "validated" : ""}`}
-          >
-            {key === editEnCours.index ? (
-              <>
-                <input
-                  type="text"
-                  value={editEnCours.text}
-                  onChange={(e) => setEditEnCours({...editEnCours, text: e.target.value})}
-                  onKeyUp={(e) => keyPressInput(e)}
-                />
-                <input
-                  type="number"
-                  placeholder="Chapitre"
-                  value={editEnCours.chapitre}
-                  onChange={(e) => setEditEnCours({...editEnCours, chapitre: e.target.value})}
-                  onKeyUp={(e) => keyPressInput(e)}
-                />
-                <button onClick={() => updateElement(key, editEnCours)}>
-                  Valider
-                </button>
-              </>
-            ) : (
-              <>
-                <p>
-                  {task.manga} : scan {task.chapitre}
-                </p>
-                <img
-                  className={'clipBoard'}
-                  src="/img/copyToClipBoard.png"
-                  onClick={() => {
-                    window.open(task.url + task.chapitre, '_blank', 'noopener,noreferrer');
-                  }}
-                />
-                <button onClick={() => startEditElement(task, key)}>Éditer</button>
-                <button onClick={() => deleteElement(key)}>&#x274C;</button>
-                <button onClick={() => isValidated(key)}>&#10003;</button>
-              </>
-            )}
-          </li>
-        ))}
+        {searched.length > 0 && (
+          <>
+            {searched.map((manga, key) => (
+              <li
+                key={key}
+                className={`listItemElement ${
+                  manga.validated ? 'validated' : ''
+                }`}
+              >
+                {key === editEnCours.index ? (
+                  <>
+                    <input
+                      type="text"
+                      value={editEnCours.text}
+                      onChange={e =>
+                        setEditEnCours({ ...editEnCours, text: e.target.value })
+                      }
+                      onKeyUp={e => keyPressInput(e)}
+                    />
+                    <input
+                      type="number"
+                      placeholder="Chapitre"
+                      value={editEnCours.chapitre}
+                      onChange={e =>
+                        setEditEnCours({
+                          ...editEnCours,
+                          chapitre: e.target.value,
+                        })
+                      }
+                      onKeyUp={e => keyPressInput(e)}
+                    />
+                    <button onClick={() => updateElement(key, editEnCours)}>
+                      Valider
+                    </button>
+                  </>
+                ) : (
+                  <>
+                    <p>
+                      {manga.manga} : scan {manga.chapitre}
+                    </p>
+                    <img
+                      className={'clipBoard'}
+                      src="/img/copyToClipBoard.png"
+                      onClick={() => {
+                        window.open(
+                          manga.url + manga.chapitre,
+                          '_blank',
+                          'noopener,noreferrer'
+                        );
+                      }}
+                    />
+                    <button onClick={() => startEditElement(manga, key)}>
+                      Éditer
+                    </button>
+                    <button onClick={() => deleteElement(key)}>&#x274C;</button>
+                    <button onClick={() => isValidated(key)}>&#10003;</button>
+                  </>
+                )}
+              </li>
+            ))}
+          </>
+        )}
+        {searched.length === 0 && (
+          <>
+            {item.map((manga, key) => (
+              <li
+                key={key}
+                className={`listItemElement ${
+                  manga.validated ? 'validated' : ''
+                }`}
+              >
+                {key === editEnCours.index ? (
+                  <>
+                    <input
+                      type="text"
+                      value={editEnCours.text}
+                      onChange={e =>
+                        setEditEnCours({ ...editEnCours, text: e.target.value })
+                      }
+                      onKeyUp={e => keyPressInput(e)}
+                    />
+                    <input
+                      type="number"
+                      placeholder="Chapitre"
+                      value={editEnCours.chapitre}
+                      onChange={e =>
+                        setEditEnCours({
+                          ...editEnCours,
+                          chapitre: e.target.value,
+                        })
+                      }
+                      onKeyUp={e => keyPressInput(e)}
+                    />
+                    <button onClick={() => updateElement(key, editEnCours)}>
+                      Valider
+                    </button>
+                  </>
+                ) : (
+                  <>
+                    <p>
+                      {manga.manga} : scan {manga.chapitre}
+                    </p>
+                    <img
+                      className={'clipBoard'}
+                      src="/img/copyToClipBoard.png"
+                      onClick={() => {
+                        window.open(
+                          manga.url + manga.chapitre,
+                          '_blank',
+                          'noopener,noreferrer'
+                        );
+                      }}
+                    />
+                    <button onClick={() => startEditElement(manga, key)}>
+                      Éditer
+                    </button>
+                    <button onClick={() => deleteElement(key)}>&#x274C;</button>
+                    <button onClick={() => isValidated(key)}>&#10003;</button>
+                  </>
+                )}
+              </li>
+            ))}
+          </>
+        )}
       </ul>
-      <ExportToJson list={item}/>
+      <ExportToJson list={item} />
       <div>
-        api pour les noms de manga = autocompletion //
-        fonction de recherche //
+        api pour les noms de manga = autocompletion // fonction de recherche //
         add dragable file //
       </div>
     </div>
@@ -173,4 +318,3 @@ const TodoList: React.FC = () => {
 };
 
 export default TodoList;
-
