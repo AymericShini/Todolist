8dc20f26863e4d5b06f64e527c6db2c1a987ad7a
AymericCliq <a.demange@cliqdigital.com>
2023-03-22 18:24:35 +0100

    feat(json): import export json

---
+++ src/components/json/exportJson.tsx
@@ -0,0 +1,24 @@
+import React, { FC } from 'react';
+
+type Props = {
+  list: object;
+};
+
+const ExportToJson: FC<Props> = ({ list }) => {
+  const exportData = () => {
+    const jsonString = `data:text/json;chatset=utf-8,${encodeURIComponent(
+      JSON.stringify(list)
+    )}`;
+    const link = document.createElement("a");
+    link.href = jsonString;
+    link.download = "data.json";
+    link.click();
+  };
+
+  return (
+    <button type="button" onClick={exportData}>
+      Export your list (Json)
+    </button>
+  );
+};
+export default ExportToJson;
---
+++ src/components/json/importJson.tsx
@@ -0,0 +1,29 @@
+import { FC } from 'react';
+import { Task } from '../todolist/todolist';
+
+type Props = { 
+  setItem: (value: object) => void;
+};
+
+const ImportJSON: FC<Props> = ({ setItem }) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const fileReader = new FileReader();
+    if (e.target.files !== null) {
+      fileReader.readAsText(e.target.files[0], "UTF-8");
+    }
+    fileReader.onload = e => {
+      let value: [] = JSON.parse(e.target.result)
+      setItem((prevState : []) => {
+        return (
+          [...prevState,
+          ...value.filter((item: Task) => prevState.some((prevItem: Task) => item.manga !== prevItem.manga))]
+        )
+      })
+    };
+  };
+
+  return (
+    <input type="file" onChange={(e) => handleChange(e)}></input>
+  );
+};
+export default ImportJSON;
