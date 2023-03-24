c3c3617aaae82e4560085ae5b3ebe0a368f97515
aymericCliq <a.demange@cliqdigital.com>
2023-03-24 12:20:10 +0100

    feat(api): added an apicall to autosuggest manga

--- src/components/json/importJson/importJson.tsx
+++ src/components/json/importJson/importJson.tsx
@@ -1,8 +1,8 @@
 import { FC } from 'react';
 import { Manga } from '../../todolist/todolist';
-import './importJson.css'
+import './importJson.css';
 
-type Props = { 
+type Props = {
   setItem: (value: any) => void;
 };
 
@@ -10,26 +10,26 @@ const ImportJSON: FC<Props> = ({ setItem }) => {
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const fileReader: any = new FileReader();
     if (e.target.files !== null) {
-      fileReader.readAsText(e.target.files[0], "UTF-8");
+      fileReader.readAsText(e.target.files[0], 'UTF-8');
     }
     fileReader.onload = (e: any) => {
-      let value: any = JSON.parse(e.target.result)
+      let value: any = JSON.parse(e.target.result);
+      value = value.filter((item: any, index: number, self: any) => index === self.findIndex((t: any) => t.save === item.save && t.manga === item.manga));
       setItem((prevState: any) => {
         if (prevState.length !== 0) {
-          value = value.filter((item: Manga) => prevState.some((prevItem: Manga) => item.manga !== prevItem.manga))
+          value = value.filter((item: Manga) => prevState.some((prevItem: Manga) => item.manga.toLowerCase() !== prevItem.manga.toLowerCase()));
         }
-        return (
-          [...prevState,
-          ...value]
-        )
-      })
+        return [...prevState, ...value];
+      });
     };
   };
 
   return (
     <>
-      <label draggable="true" htmlFor="file"  className="label-file">Choisi un fichier JSON</label>
-      <input draggable="true" className="input-file" id='file' type="file" onChange={(e) => handleChange(e)}></input>
+      <label htmlFor="file" className="label-file">
+        Choisi un fichier JSON
+      </label>
+      <input draggable="true" className="input-file" id="file" type="file" onChange={e => handleChange(e)}></input>
     </>
   );
 };
